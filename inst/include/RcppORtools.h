aa3e07c63a421b9a66d5db8ad7359558f3eb978c
Adam Sardar <adam@sardar.me.uk>
2019-05-24 16:25:02 +0100

    Ensure R does not export macro defs that clash names

--- inst/include/RcppORtools.h
+++ inst/include/RcppORtools.h
@@ -4,4 +4,8 @@
 // Looks like ERROR macro is already defined elsehwere in R
 #define GLOG_NO_ABBREVIATED_SEVERITIES
 
+// R headers define macros with very generic names, such as Free for S-PLUS back compatibility. STRICT_R_HEADERS disables this.
+// https://github.com/dahtah/imager/issues/7
+#define STRICT_R_HEADERS
+
 #endif
