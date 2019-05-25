9db4d246f62ee7d2b68ec21c4bc81008a750113b
Adam Sardar <adam@sardar.me.uk>
2019-05-25 14:33:03 +0100

    Pull utilitymethod that wraps IntegerMatrix out into its own header-only file

---
+++ src/mat2stdVecVec.h
@@ -0,0 +1,22 @@
+#include <RcppORtools.h>
+#include <Rcpp.h> 
+using namespace Rcpp;
+
+#include <ortools/base/integral_types.h>
+
+// The std::vector< std::vector<int64>> structure is used alot throughout ORtools.
+// I did consider using a templated function, which necessitated RcppArmadillo, but integer types are used throughout ORtools.
+std::vector<std::vector<int64>> matrixToStdVecVec(IntegerMatrix mat2convert){
+  
+  std::vector< std::vector<int64>> vecvec(mat2convert.nrow());
+  
+  //Alot of copying going on. This is probably fine, because the branch & bound process will dominate time.
+  for(size_t i = 0; i < mat2convert.nrow(); ++i){
+    
+    IntegerVector intMatRow = mat2convert.row(i);
+    vecvec[i] = as<std::vector<int64>>(intMatRow);
+  }
+  
+  return vecvec;
+}
+// This should probably be moved in to the includes at some stage. Think about Rcpp Honey
--- src/solveKnapsack.cpp
+++ src/solveKnapsack.cpp
@@ -5,23 +5,7 @@ using namespace Rcpp;
 
 #include <ortools/algorithms/knapsack_solver.h>
 
-// The std::vector< std::vector<int64>> structure is used alot throughout ORtools.
-// I did consider using a templated function, which necessitated RcppArmadillo, but integer types are used throughout ORtools.
-std::vector<std::vector<int64>> matrixToStdVecVec(IntegerMatrix mat2convert){
-
-  std::vector< std::vector<int64>> vecvec(mat2convert.nrow());
-
-  //Alot of copying going on. This is probably fine, because the branch & bound process will dominate time.
-  for(size_t i = 0; i < mat2convert.nrow(); ++i){
-
-    IntegerVector intMatRow = mat2convert.row(i);
-    vecvec[i] = as<std::vector<int64>>(intMatRow);
-  }
-
-  return vecvec;
-}
-// This should probably be moved in to the includes at some stage. Think about Rcpp Honey
-
+#include "./mat2stdVecVec.h"
 
 //' A simple knapsack solver
 //' 
